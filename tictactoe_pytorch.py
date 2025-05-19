401a7b81b7b726c40fdd29fedf3b0a46a849992b
Michael Pan <michael.pan@kooapps.com>
2025-05-19 16:21:33 +0800

    Fix the training & add console tic tac toc to test

--- tictactoe_pytorch.py
+++ tictactoe_pytorch.py
@@ -2,6 +2,7 @@ import csv
 import torch
 import pandas
 import os
+import curses
 
 from pprint import pprint
 from torch.utils.data import Dataset, DataLoader
@@ -56,6 +57,20 @@ class TicTacToeBoard:
 
     def to_csv_row(self):
         return [x for x in self.board] + [self.get_winner()]
+    
+    def get_line_with_select_symbol(self, selected_index: tuple[int, int]):
+        lines = []
+
+        for i in range(3):
+            line = ""
+            for j in range(3):
+                if (i, j) == selected_index:
+                    line += f"[{self.SYMBOLS[self.board[i * 3 + j]]}]"
+                else:
+                    line += f" {self.SYMBOLS[self.board[i * 3 + j]]} "
+            lines.append(line)
+        
+        return lines
 
 
 def create_trainning_data():
@@ -119,7 +134,7 @@ class NeuralNetworkModel(torch.nn.Module):
             torch.nn.Linear(128, 32),
             torch.nn.ReLU(),
             torch.nn.Dropout(0.1),
-            torch.nn.Linear(32, 1),
+            torch.nn.Linear(32, 3),
         )
 
     def forward(self, x):
@@ -140,7 +155,10 @@ class NeuralNetworkController:
         self.model = NeuralNetworkModel().to(self.device)
 
         if os.path.exists(self.save_file_name):
-            self.model.load_state_dict(torch.load(self.save_file_name))
+            try:
+                self.model.load_state_dict(torch.load(self.save_file_name))
+            except Exception as e:
+                pass
     
     def start_training(self):
         self.model.train()
@@ -148,7 +166,7 @@ class NeuralNetworkController:
         loss_fn = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
 
-        for epoch in range(10):
+        for epoch in range(20):
             for batch, label in self.data_loader:
                 X, y = batch.to(self.device), label.to(self.device)
 
@@ -165,7 +183,7 @@ class NeuralNetworkController:
 
         torch.save(self.model.state_dict(), "tictactoe_model.pth")
 
-    def test(self):
+    def evaluate_accuracy(self):
         loss_fn = torch.nn.CrossEntropyLoss()
 
         size = len(self.data_loader.dataset)
@@ -181,12 +199,106 @@ class NeuralNetworkController:
         test_loss /= num_batches
         correct /= size
         print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+    
+    def check_board_result(self, board: TicTacToeBoard):
+        ai_prediction = self.model(torch.tensor([board.board]).float().to(self.device))
+        return ai_prediction.argmax(1).item()
+    
+    def test_data(self):
+        result = self.model(torch.tensor([[0, 0, 0, 0, 0, 0, 0, 0, 0]]).float().to(self.device))
+        print(result.argmax(1).item())
+
+        result = self.model(torch.tensor([[1, 1, 1, 0, 0, 0, 0, 0, 0]]).float().to(self.device))
+        print(result.argmax(1))
+
+        result = self.model(torch.tensor([[2, 2, 2, 0, 0, 0, 0, 0, 0]]).float().to(self.device))
+        print(result.argmax(1))
+
+
+class TicTacToeGame:
+    instance = None
+
+    @staticmethod
+    def static_run(stdscr):
+        TicTacToeGame.instance.run(stdscr)
+
+    def __init__(self, network: NeuralNetworkController):
+        TicTacToeGame.instance = self
+
+        self.network = network
+        self.board = TicTacToeBoard()
+        self.current_player = 1
+
+        self.selected_index = (0, 0)
+
+        self.stdscr = None
+    
+    def run(self, stdscr):
+        self.stdscr = stdscr
+
+        self.draw_board()
+
+        while True:
+            try:
+                key = stdscr.getch()
+            except KeyboardInterrupt:
+                break
+
+            self.handle_input(key)
+            self.draw_board()
+    
+    def draw_board(self):
+        self.stdscr.clear()
+
+        lines = self.board.get_line_with_select_symbol(self.selected_index)
+        for i, line in enumerate(lines):
+            self.stdscr.addstr(i, 0, line)
+        
+        actual_winner = self.board.get_winner()
+        self.stdscr.addstr(4, 0, f"Actual Winner: {TicTacToeBoard.SYMBOLS[actual_winner]}")
+
+        predict_winner = self.network.check_board_result(self.board)
+        self.stdscr.addstr(5, 0, f"Predict Winner: {TicTacToeBoard.SYMBOLS[predict_winner]}")
+        
+        self.stdscr.refresh()
+    
+    def handle_input(self, key: int):
+        if key == 258:  # Down
+            self.selected_index = (self.selected_index[0] + 1, self.selected_index[1])
+            if self.selected_index[0] > 2:
+                self.selected_index = (0, self.selected_index[1])
+        elif key == 259:  # Up
+            self.selected_index = (self.selected_index[0] - 1, self.selected_index[1])
+            if self.selected_index[0] < 0:
+                self.selected_index = (2, self.selected_index[1])
+        elif key == 260:  # Left
+            self.selected_index = (self.selected_index[0], self.selected_index[1] - 1)
+            if self.selected_index[1] < 0:
+                self.selected_index = (self.selected_index[0], 2)
+        elif key == 261:  # Right
+            self.selected_index = (self.selected_index[0], self.selected_index[1] + 1)
+            if self.selected_index[1] > 2:
+                self.selected_index = (self.selected_index[0], 0)
+        elif key == 10:  # Enter
+            self.board.set_piece(self.selected_index, self.current_player)
+            if self.current_player == 1:
+                self.current_player = 2
+            else:
+                self.current_player = 1
+
+    
 
 
 if __name__ == "__main__":
     controller = NeuralNetworkController()
-    controller.test()
+    controller.evaluate_accuracy()
     controller.start_training()
-    controller.test()
+    controller.evaluate_accuracy()
+    # controller.test_data()
+
+    # game = TicTacToeGame(controller)
+    # curses.wrapper(TicTacToeGame.static_run)
+
+
 
 
